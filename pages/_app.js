2edd8b6c8c1320c52c29b4226b1a5224cbb0b2a5
popsps <59066663+popsps@users.noreply.github.com>
2020-06-20 14:56:51 -0400

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import "../styles/main.css"
+// import "../styles/main.css"
 import {ProductProvider} from "../productContext";
 import React from "react";
 
-export default function MyApp({Component, pageProps}) {
+export default function App({Component, pageProps}) {
   return (
     <ProductProvider>
       <Component {...pageProps}/>
