5afa81cd944f390ec5baf145a5dd27041b2f1256
popsps <59066663+popsps@users.noreply.github.com>
2020-06-20 14:54:51 -0400

    test

--- pages/cart.js
+++ pages/cart.js
@@ -12,8 +12,8 @@ const Cart = () => {
       <p>Your cart is empty</p>
     </div>
   );
-  console.log('PAYPAL_ID:', process.env.PAYPAL_ID)
-  console.log('NEXT_PUBLIC_PAYPAL_ID:', process.env.NEXT_PUBLIC_PAYPAL_ID)
+  console.log('---PAYPAL_ID:', process.env.PAYPAL_ID)
+  console.log('---NEXT_PUBLIC_PAYPAL_ID:', process.env.NEXT_PUBLIC_PAYPAL_ID)
   if (cart.length > 0) {
     cartDisplay = (
       <div className='text-center'>
