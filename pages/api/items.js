6d231dd11e490c4dfeb7ba7ee533d10bdfe93928
popsps <59066663+popsps@users.noreply.github.com>
2020-06-21 16:03:34 -0400

    api/items

---
+++ pages/api/items.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+console.log('feefe')
+const mongoURI = process.env.MONGO_KEY
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+  .then(() => console.log('mongo db connected'))
+  .catch(err => console.log("error connection"))
+// const mongoConnection = await mongoose.createConnection(mongoURI)
+
+const ProductSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+
+let Product
+
+
+export default (req, res) => {
+  // try {
+  //   Product = mongoose.model('product')
+  //   console.log('already in')
+  // } catch (err) {
+  //   console.log('get a new schema')
+  //   Product = mongoose.model('product', ProductSchema)
+  // }
+
+  try {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json')
+
+    res.json({val: 12323})
+
+  } catch (err) {
+    console.log('something went wrong...')
+    res.statusCode = 404
+    res.end(err)
+  }
+}
+
