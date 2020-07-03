74c0bc01a871ce02740bae34cc3b5871421b60bb
popsps <59066663+popsps@users.noreply.github.com>
2020-07-03 15:57:35 -0400

    fixed nav mobile

--- components/navigation.js
+++ components/navigation.js
@@ -2,18 +2,31 @@ import React, {Component} from 'react';
 import Link from "next/link";
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false
+    }
+    this.nav = React.createRef()
+  }
+
   render() {
+    const {collapsed} = this.state
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link href='/'><a className="navbar-brand">Products</a></Link>
         <button className="navbar-toggler" type="button"
                 data-toggle="collapse" data-target="#navbarColor03"
                 aria-controls="navbarColor03" aria-expanded="false"
-                aria-label="Toggle navigation">
+                aria-label="Toggle navigation"
+                onClick={() => {
+                  this.nav.current.classList.toggle('collapse')
+                  this.setState({collapsed: !collapsed})
+                }}>
           <span className="navbar-toggler-icon"/>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarColor03">
+        <div className="collapse navbar-collapse" id="navbarColor03" ref={this.nav}>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
               <Link href='/'>
@@ -26,9 +39,9 @@ class Navigation extends Component {
               </Link>
             </li>
           </ul>
-          <div className="nav-item">
+          <div className="nav-item" >
             <Link href='/cart'>
-              <a className="nav-link">
+              <a className="nav-link" >
                 <i className='fas fa-cart-plus mr-1'/>
                 My Cart
               </a>
